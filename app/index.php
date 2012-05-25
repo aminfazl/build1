89f9f83917feba2c169882d7142cefcc750e9d1c
ziyao <yangziyao0824@hotmail.com>
2012-05-25 16:13:40 +1000

    Revert "test"
    This reverts commit 612a56b2edc3bc044e31879d76fcacaa30d44314.

--- app/index.php
+++ app/index.php
@@ -13,6 +13,5 @@
  * @package       app
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- *
  */
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
